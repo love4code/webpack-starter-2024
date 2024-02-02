ded5a3ef0d6a685123d6acfabd6c479f3336aa09
Mark Grover <markgrover@Marks-MacBook-Air.local>
2024-02-01 22:27:03 -0500

    added bootstrap5

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import generateJoke from './generateJoke'
 import './styles/main.scss'
 import laughing from './assets/laughing.svg'
 import 'bootstrap';
-import 'bootstrap/functions';
+
 
 const laughImg = document.getElementById('laughImg')
 laughImg.src = laughing
